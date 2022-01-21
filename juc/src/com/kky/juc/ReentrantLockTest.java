02d1b360b45f79ccc8819e2324d3cac767310403
kekaiyuan <1054623598@qq.com>
2022-01-21 12:01:59 +0800

    1.21

--- juc/src/com/kky/juc/ReentrantLockTest.java
+++ juc/src/com/kky/juc/ReentrantLockTest.java
@@ -1,6 +1,7 @@
 package com.kky.juc;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -10,10 +11,10 @@ public class ReentrantLockTest {
     /**
      * 普通上锁 lock()
      */
-    public void lock01(){
+    public void lock01() {
         try {
             lock.lock(); //上锁
-			//业务逻辑
+            //业务逻辑
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -24,17 +25,17 @@ public class ReentrantLockTest {
     /**
      * 非阻塞式上锁 tryLock()
      */
-    public void lock02(){
+    public void lock02() {
         boolean locked = false;
         try {
             //locked = lock.tryLock();
             locked = lock.tryLock(5, TimeUnit.SECONDS);
             if (locked) {
                 System.out.println("获得锁成功");
-			    //成功逻辑
+                //成功逻辑
             } else {
                 System.out.println("获得锁失败");
-			    //失败逻辑
+                //失败逻辑
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -49,19 +50,16 @@ public class ReentrantLockTest {
      * 可响应外部中断上锁 lockInterruptibly()
      */
     public void lock03() throws InterruptedException {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    lock.lockInterruptibly();
-                    System.out.println(Thread.currentThread().getName()+" start");
-                    Thread.sleep(Integer.MAX_VALUE);
-                    System.out.println(Thread.currentThread().getName()+" end");
-                } catch (InterruptedException e) {
-                    System.out.println(Thread.currentThread().getName()+" interrupted!");
-                } finally {
-                    lock.unlock();
-                }
+        Runnable runnable = () -> {
+            try {
+                lock.lockInterruptibly();
+                System.out.println(Thread.currentThread().getName() + " start");
+                Thread.sleep(Integer.MAX_VALUE);
+                System.out.println(Thread.currentThread().getName() + " end");
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " interrupted!");
+            } finally {
+                lock.unlock();
             }
         };
 
@@ -78,9 +76,47 @@ public class ReentrantLockTest {
         t2.interrupt(); //打断线程 2 的等待
     }
 
+    /**
+     * 实现 Condition 变量实现线程同步
+     */
+    public void condition() throws InterruptedException {
+        Condition condition1 = lock.newCondition();
+        Condition condition2 = lock.newCondition();
+        Runnable runnable1 = () -> {
+            lock.lock();
+            try {
+                condition1.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println(Thread.currentThread().getName());
+        };
+        Runnable runnable2 = () -> {
+            lock.lock();
+            try {
+                condition2.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+            System.out.println(Thread.currentThread().getName());
+        };
+        for (int i = 0; i < 5; i++) {
+            new Thread(runnable1, "t1-" + i).start();
+            new Thread(runnable2, "t2-" + i).start();
+        }
+        Thread.sleep(1000);
+        lock.lock();
+        condition1.signalAll();
+        lock.unlock();
+    }
+
     public static void main(String[] args) throws InterruptedException {
         ReentrantLockTest reentrantLockTest = new ReentrantLockTest();
-        reentrantLockTest.lock03();
+        reentrantLockTest.condition();
     }
 }
 
